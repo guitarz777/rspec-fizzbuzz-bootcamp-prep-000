bf136098ba0014b1f995aa002f975f9a7b236bbb
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-12 21:20:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,10 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 ==0 
     return 'fizzbuzz'
+  end
   elsif number % 3 == 0 
     return 'fizz'
+  end
   elsif number % 5 == 0 
     return 'buzz'
   else
